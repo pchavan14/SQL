377d130b4982ec4d46bdc9e24099860b2b2dfb22
Prachi Chavan <prachichavan@Prachis-MacBook-Pro.local>
2023-03-19 19:05:18 -0700

    adding new query

--- queries.sql
+++ queries.sql
@@ -5,3 +5,17 @@ left join
 orders b
 on a.cust_code = b.cust_code
 where b.cust_code is NULL
+
+--sql query to get max count record in top ten records
+with cte as
+(
+select cust_name , OUTSTANDING_AMT
+from customer
+order by OUTSTANDING_AMT desc
+limit 10
+)
+select cust_name , count(OUTSTANDING_AMT)
+from cte
+group by cust_name
+order by count(OUTSTANDING_AMT) desc
+limit 1
