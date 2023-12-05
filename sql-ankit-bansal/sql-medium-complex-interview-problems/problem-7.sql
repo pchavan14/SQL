eb6d8dee6a36d7d09623bba4027c914d728bf408
Prachi Chavan <prchavan1412@gmail.com>
2023-12-05 15:29:45 -0800

    new SQL problems

---
+++ sql-ankit-bansal/sql-medium-complex-interview-problems/problem-7.sql
@@ -0,0 +1,21 @@
+-- most asked join questions
+
+-- first table - 5 records
+-- second table - 10 records
+
+-- any value in each of the table , how many max , min records for each type of join 
+-- max when all the values of both the tables is same 
+-- min when both the tables have different values
+
+ex.
+A    b
+1       2
+1       2
+1       2
+
+A     b
+1       1
+1       1
+1       1
+
+
